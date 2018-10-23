a8cff974824172c4b2a22e64eb9a9f4d4117fc6f
Yevhenii Kyshko <yevhenii.kyshko@vistex.com>
2018-10-23 14:58:23 +0200

    ReservationsController

--- PanKanapka/Api.Web/Controllers/ReservationsController.cs
+++ PanKanapka/Api.Web/Controllers/ReservationsController.cs
@@ -2,12 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Components;
+using Api.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Web.Controllers
 {
     public class ReservationsController : Controller
     {
+        public void DeleteReservations(IEnumerable<string> reservations)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Reservation> GetReservations(ReservationFilter reservationsFilter)
+        {
+            return new List<Reservation>
+            {
+                new Reservation()
+                {
+                    ClientFirmId = "client",
+                    ReservationId = "resId",
+                    IsActiveReservation = true,
+                    ReservationItems = new List<ReservationItem>
+                    {
+                        new ReservationItem()
+                        {
+                            Name = "bulka",
+                            PriceTotal = 456.34,
+                            ItemImageUrl = null,
+                            Quantity = 3
+                        }
+                    }
+                }
+            };
+        }
+
         public IActionResult Index()
         {
             return View();
