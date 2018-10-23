e82fe9b8c593464cabf7842e1b44ad03d8d86e47
Yevhenii Kyshko <yevhenii.kyshko@vistex.com>
2018-10-23 14:55:27 +0200

    Db.Contracts project added.

---
+++ PanKanapka/Db.Contracts/Models/TaskDbResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Db.Contracts.Models
+{
+    public class TaskDbResult
+    {
+       /* select w.name, w.surname, t.taskDate, cf.logoUrl, cf.name, cf.address, t.isDone
+  from Tasks t
+  join Workers w on w.ID = t.workerID
+  join ClientFirms cf on cf.ID = t.clientFirmID
+  where w.ID in ('workerIds') and t.taskDate in (datediff(DAY, 1, 'date'), 'date', dateadd(DAY, 1, 'date'))
+  */
+    }
+}
---
+++ PanKanapka/Db.Contracts/TasksRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Api.Domain.Components;
+using Api.Domain.Models;
+using Dapper;
+
+namespace Db.Contracts
+{
+    public class TasksRepository : ITasksRepository
+    {
+        public void CreateTasks(IEnumerable<TaskCreationItem> tasksItems)
+        {
+            string insertTaskSqlQuery = "insert into Tasks values(@ClientFirmId, 0, @Date, @WorkerId)";
+
+            //Change DataSource to ./SQLEXPRESS
+            using (IDbConnection dbConn = new SqlConnection(@"Integrated Security = SSPI; Persist Security Info = False; Initial Catalog = Projekt; Data Source =.;"))
+            {
+                dbConn.Open();
+
+                var affectedRows = dbConn.Execute(insertTaskSqlQuery,
+                tasksItems
+                .SelectMany(p =>
+                {
+                    return p.ClientFirmIds
+                         .Select(fId =>
+                         {
+                             return new
+                             {
+                                 ClientFirmId = long.Parse(fId),
+                                 p.Date,
+                                 WorkerId = long.Parse(p.WorkerId)
+                             };
+                         });
+                })
+                );
+            }
+        }
+
+        public void DeleteTasks(IEnumerable<string> taskIds)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Task> GetTasks(TasksFilter tasksFilter)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
