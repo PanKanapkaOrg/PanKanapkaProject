397e5d6e188bec208cccf1eea540fc8c149c7d5b
LukaszStarus <209464@edu.p.lodz.pl>
2018-11-30 09:42:07 +0100

    Login API

--- PanKanapka/Db.Contracts/FoodsDbRepository.cs
+++ PanKanapka/Db.Contracts/FoodsDbRepository.cs
@@ -4,7 +4,6 @@ using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Db.Contracts
---
+++ PanKanapka/Db.Contracts/LoginDbRepository.cs
@@ -0,0 +1,35 @@
+﻿using Api.Domain.Components;
+using Api.Domain.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Db.Contracts
+{
+    public class LoginDbRepository : ILoginRepository
+    {
+        private IDbConnection DbConn;
+        public LoginDbRepository(IDbConnection dbConnection)
+        {
+            DbConn = dbConnection;
+        }
+        public async Task<Login> GetLoginData(string mail, string password)
+        {
+            string getLoginDataProcedure = @"LoginProcedure";
+            using (DbConn)
+            {
+                DbConn.Open();
+                if(DbConn.State!= ConnectionState.Open)
+                {
+                    throw new Exception("Nie udało się polączyć z bazą");
+                }
+                IEnumerable<Login> loginData = DbConn.Query<Login>(getLoginDataProcedure,new { mail, password }, commandType: CommandType.StoredProcedure);
+                return loginData.FirstOrDefault();
+            }
+        }
+    }
+}
